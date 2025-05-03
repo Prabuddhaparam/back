f21b9f895789b6b5eb02f8948c900c6800035477
Ashutosh <ashu6549oajhd@gmail.com>
2025-05-03 12:15:00 +0530

    add gitignore file and debug in db connection

--- src/db/index.js
+++ src/db/index.js
@@ -4,7 +4,7 @@ import { DB_NAME } from '../constant.js';
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(`${process.env.DATABASE_URL} `);
+        const conn = await mongoose.connect(`${process.env.DATABASE_URL}/${DB_NAME}`);
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
         console.error(`Error: ${error.message}`);
